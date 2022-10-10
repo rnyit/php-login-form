9675cfb37aba38e97310e183392741f5453e4b8f
rnyweb <49764738+rnyweb@users.noreply.github.com>
2022-10-10 08:10:01 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+////////////////
+// CONFIG.PHP //
+////////////////
+
+// Set username and password for the login form dashboard.
+define('username', 'user');
+define('password', 'Pass321');
