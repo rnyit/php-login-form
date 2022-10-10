c82a08e3b40276f7fa2d673ce174b6409c7c2460
rnyweb <49764738+rnyweb@users.noreply.github.com>
2022-10-10 08:10:38 +0300

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+// Start new or resume existing session.
+session_start();
+
+// Unset a given variable, in this case the username.
+unset($_SESSION['user']);
+
+// Session variable.
+$_SESSION['message'] = 'Logged out.';
+
+// Redirect to another location.
+header('Location: login.php');
+// Terminate the current script.
+exit();
