9d3c8f381e8a47364faff21f905496986a6b0241
rnyweb <49764738+rnyweb@users.noreply.github.com>
2022-10-10 09:21:21 +0300

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,28 @@
+<?php
+// Required file.
+require 'config.php';
+
+// Start new or resume existing session.
+session_start();
+
+// Check if not a user is logged in.
+if(!isset($_SESSION['user'])) {
+  // Session variable.
+  $_SESSION['message'] = 'Access is required.';
+  // Redirect to another location.
+  header('Location: login.php');
+  // Terminate the current script.
+  exit();
+}
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Dashboard</title>
+  </head>
+  <body>
+    <p>Hello, <?php echo $_SESSION['user']; ?></p>
+    <p><a href="dashboard.php">[Dashboard]</a> <a href="logout.php">[Logout]</a></p>
+  </body>
+</html>
