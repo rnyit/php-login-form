2680660a6602592a2f2e83712c9229563e07b762
rnyweb <49764738+rnyweb@users.noreply.github.com>
2022-11-28 11:58:14 +0200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,5 @@
 <?php
-// Required file.
+// Required file(s).
 require 'config.php';
 
 // Start new or resume existing session.
@@ -9,8 +9,10 @@ session_start();
 if(!isset($_SESSION['user'])) {
   // Session variable.
   $_SESSION['message'] = 'Access is required.';
+  
   // Redirect to another location.
   header('Location: login.php');
+  
   // Terminate the current script.
   exit();
 }
